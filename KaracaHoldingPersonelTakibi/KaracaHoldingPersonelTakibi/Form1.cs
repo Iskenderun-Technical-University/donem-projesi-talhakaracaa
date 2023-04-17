ac14ce33072722f22ff4155a083508dd5cb63794
talhakaracaa <zkaraca.mdbf20@iste.edu.tr>
2023-04-17 17:00:29 +0300

    Güncelleme

---
+++ KaracaHoldingPersonelTakibi/KaracaHoldingPersonelTakibi/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+namespace KaracaHoldingPersonelTakibi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Veritabanı dosya yolu
+
+        OleDbConnection baglantim = new OleDbConnection("Provider=Microsoft.Ace.OleDb.12.0;Data Source=PersonelBilgileri.accdb");
+
+        // Formlar Arası Değişkenler
+
+        int hak = 3;bool durum = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = " Kullanıcı Girişi";
+            this.AcceptButton = button1;
+            this.CancelButton = button2;
+            label5.Text = Convert.ToString(hak);
+            radioButton1.Checked = true;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+        }
+    }
+}

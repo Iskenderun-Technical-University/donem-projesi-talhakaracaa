be7924052fd85ad8eb08f8749612597aa6a9a9c6
talhakaracaa <zkaraca.mdbf20@iste.edu.tr>
2023-04-19 11:45:38 +0300

    Veri Tabanı Bağlantısı

--- KaracaHoldingPersonelTakibi/KaracaHoldingPersonelTakibi/Form1.Designer.cs
+++ KaracaHoldingPersonelTakibi/KaracaHoldingPersonelTakibi/Form1.Designer.cs
@@ -120,6 +120,7 @@
             this.button1.TabIndex = 7;
             this.button1.Text = "Giriş";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
--- KaracaHoldingPersonelTakibi/KaracaHoldingPersonelTakibi/Form1.cs
+++ KaracaHoldingPersonelTakibi/KaracaHoldingPersonelTakibi/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.OleDb;
+using System.IO.Ports;
+
 namespace KaracaHoldingPersonelTakibi
 {
     public partial class Form1 : Form
@@ -16,13 +18,17 @@ namespace KaracaHoldingPersonelTakibi
         {
             InitializeComponent();
         }
-        //Veritabanı dosya yolu
+        //Veritabanı dosya yolu ve provider nesnesinin belirlenmesi 
 
         OleDbConnection baglantim = new OleDbConnection("Provider=Microsoft.Ace.OleDb.12.0;Data Source=PersonelBilgileri.accdb");
 
-        // Formlar Arası Değişkenler
+        // Formlar arası veri aktarımında kullanılacak değişkenler
+        public static string tcno, adi, soyadi, yetki;
 
-        int hak = 3;bool durum = false;
+        // Yerel yani yalnızca bu formda geçerli olacak değişkenler 
+
+        int hak = 3; bool durum = false;
+
         private void Form1_Load(object sender, EventArgs e)
         {
             this.Text = " Kullanıcı Girişi";
@@ -33,5 +39,65 @@ namespace KaracaHoldingPersonelTakibi
             this.StartPosition = FormStartPosition.CenterScreen;
             this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (hak != 0)
+            {
+                baglantim.Open();
+                OleDbCommand selectsorgu = new OleDbCommand("select * from kullanicilar", baglantim);
+                OleDbDataReader kayitokuma = selectsorgu.ExecuteReader();
+                while (kayitokuma.Read())
+                {
+                    if (radioButton1.Checked == true)
+                    {
+                        if (kayitokuma["kullaniciadi"].ToString() == textBox1.Text && kayitokuma["parola"].ToString() == textBox2.Text && kayitokuma["yetki"].ToString() == "Yönetici")
+                        {
+                            durum = true;
+                            tcno = kayitokuma.GetValue(0).ToString();
+                            adi = kayitokuma.GetValue(1).ToString();
+                            soyadi = kayitokuma.GetValue(2).ToString();
+                            yetki = kayitokuma.GetValue(3).ToString();
+                            this.Hide();
+                            Form2 frm2 = new Form2();
+                            frm2.Show();
+                            break;
+                        }
+                    }
+
+
+
+                    if (radioButton2.Checked == true)
+                    {
+                        if (kayitokuma["kullaniciadi"].ToString() == textBox1.Text && kayitokuma["parola"].ToString() == textBox2.Text && kayitokuma["yetki"].ToString() == "Kullanıcı")
+                        {
+                            durum = true;
+                            tcno = kayitokuma.GetValue(0).ToString();
+                            adi = kayitokuma.GetValue(1).ToString();
+                            soyadi = kayitokuma.GetValue(2).ToString();
+                            yetki = kayitokuma.GetValue(3).ToString();
+                            this.Hide();
+                            Form3 frm3 = new Form3();
+                            frm3.Show();
+                            break;
+                        }
+                    }
+                }
+                if (durum == false)
+                    hak--;
+                baglantim.Close();
+            }
+            label5.Text = Convert.ToString(hak);
+            if (hak==0)
+            {
+                button1.Enabled = false;
+                MessageBox.Show("Giriş Hakkı Kalmadı!", "SKY Personel Takip Programı", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close(); 
+            }
+        }
+
+       
+
+       
     }
 }

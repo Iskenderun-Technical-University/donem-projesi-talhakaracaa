9f0ca02a1a90e93353e31da0b6b2cb98d187d54f
talhakaracaa <zkaraca.mdbf20@iste.edu.tr>
2023-05-14 13:25:26 +0300

    Form3 Oluşturuldu ve Gerekli Kodlar Yazıldı

--- KaracaHoldingPersonelTakibi/KaracaHoldingPersonelTakibi/Form3.cs
+++ KaracaHoldingPersonelTakibi/KaracaHoldingPersonelTakibi/Form3.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace KaracaHoldingPersonelTakibi
 {
@@ -15,6 +16,54 @@ namespace KaracaHoldingPersonelTakibi
         public Form3()
         {
             InitializeComponent();
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+        OleDbConnection baglantim = new OleDbConnection("Provider = Microsoft.Ace.OleDb.12.0;Data Source=PersonelBilgileri.accdb");
+        
+        private void personelleri_goster()
+        {
+            try
+            {
+                baglantim.Open();
+                OleDbDataAdapter personelleri_listele= new OleDbDataAdapter("select tcno AS [TC KİMLİK NO],ad AS[ADI], soyad AS [SOYADI],cinsiyet as [CİNSİYETİ],mezuniyet as [MEZUNİYETİ],dogumtarihi as [DOĞUM TARİHİ],gorevi as [GÖREVİ],gorevyeri as[GÖREV YERİ],maasi as [MAAŞI] from personeller Order By ad ASC", baglantim);
+                DataSet dshafıza = new DataSet();
+                personelleri_listele.Fill(dshafıza);
+                dataGridView1.DataSource = dshafıza.Tables[0];
+                baglantim.Close();
+            }
+            catch (Exception hatamsj)
+            {
+                MessageBox.Show(hatamsj.Message, "SKY Personel Takip Programı", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                baglantim.Close();
+            }
+        }
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            personelleri_goster();
+            this.Text = "KULLANICI İŞLEMLERİ";
+            label19.Text = Form1.adi + " " + Form1.soyadi;
+            pictureBox1.Height = 150; pictureBox1.Width = 150;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+
+            pictureBox2.Height = 150; pictureBox1.Width = 150;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.BorderStyle = BorderStyle.Fixed3D;
+
+            try
+            {
+                pictureBox2.Image = Image.FromFile(Application.StartupPath + "\\kullaniciresimler\\" + Form1.tcno + ".png");
+            }
+            catch
+            {
+                pictureBox2.Image = Image.FromFile(Application.StartupPath + "\\kullaniciresimler\\ resimyok.png");
+            }
+            maskedTextBox1.Mask = "00000000000";
+
         }
     }
 }
